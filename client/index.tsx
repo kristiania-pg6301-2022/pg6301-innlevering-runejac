1070de00c554dde67cf1e9ba30a58f83158503db
runejac <69790920+runejac@users.noreply.github.com>
2022-02-07 23:32:07 +0100

    2nd try Github actions (15th really)

--- client/index.tsx
+++ client/index.tsx
@@ -1,5 +1,5 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
 import Quiz from "./Quiz";
 import { BrowserRouter } from "react-router-dom";
 
